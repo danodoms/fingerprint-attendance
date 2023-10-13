f183c39fc5269894764bcab57678782801347c31
danodoms <danodoms@gmail.com>
2023-10-13 19:12:04 +0800

    added employee mgmt fields

--- src/Controller/EmployeeMgmtController.java
+++ src/Controller/EmployeeMgmtController.java
@@ -7,7 +7,11 @@ package Controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
@@ -15,6 +19,27 @@ import javafx.fxml.Initializable;
  * @author admin
  */
 public class EmployeeMgmtController implements Initializable {
+
+    @FXML
+    private TextField FnameField;
+    @FXML
+    private TextField LnameField;
+    @FXML
+    private TextField MnameField;
+    @FXML
+    private TextField emailField;
+    @FXML
+    private TextField contactNumField;
+    @FXML
+    private ChoiceBox<String> departmentChoiceBox;
+    @FXML
+    private ChoiceBox<String> positionChoiceBox;
+    @FXML
+    private ChoiceBox<String> sexChoiceBox;
+    @FXML
+    private DatePicker dateOfBirthPicker;
+    @FXML
+    private TextField addressField;
 
     /**
      * Initializes the controller class.
