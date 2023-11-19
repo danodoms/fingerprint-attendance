bb5efab430d7387e215993951f2bc326ee826cab
Grinhornet <timadong25@gmail.com>
2023-11-19 08:42:14 +0800

    Admin Attendance Report added new features.(Partial)

--- src/Controller/ADMIN_AttReportsCTRL.java
+++ src/Controller/ADMIN_AttReportsCTRL.java
@@ -5,13 +5,16 @@
 package Controller;
 
 import Model.Attendance;
+import static Model.Attendance.getAttendancebyLate;
 import static Model.Attendance.getEmpName;
 import static Model.Attendance.getYearforLabel;
+import java.time.YearMonth;
 import Utilities.DatabaseUtil;
 import Utilities.PaneUtil;
 import java.net.URL;
 import java.time.LocalDate;
 import java.time.Month;
+import java.util.Date;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
@@ -33,11 +36,13 @@ import javafx.scene.input.KeyEvent;
  */
 public class ADMIN_AttReportsCTRL implements Initializable{
     @FXML
-    private TableView<Attendance> empNameTable;
+    private TableView<Attendance> empNameTable, tardinessTable;
     @FXML
-    private TableColumn<Attendance, String> empName;
+    private TableColumn<Attendance, String> empName, notCol, arCol;
     @FXML
-    private Label monthYearLabel, nameLabel, dateTimeLabel;
+    private TableColumn<Attendance, Date> dateCol;
+    @FXML
+    private Label monthYearLabel, nameLabel, dateTimeLabel, totalLogin, totalTardiness, byPercent, totalLate;
     @FXML
     private ChoiceBox <String> monthChoiceBox;
     @FXML
@@ -45,20 +50,29 @@ public class ADMIN_AttReportsCTRL implements Initializable{
     @FXML
     private TextField searchBar;
     @FXML
-    private Button resetBtn;
+    private Button resetBtn, selectBtn;
+    
     
     DatabaseUtil dbMethods = new DatabaseUtil();
     PaneUtil method = new PaneUtil();
     LocalDate currentDate = LocalDate.now();
     int currentYear = currentDate.getYear();
     Month currentMonth = currentDate.getMonth();
+    int currentDay = currentDate.getDayOfMonth();
     String capitalizedMonth = currentMonth.toString().substring(0, 1) + currentMonth.toString().substring(1).toLowerCase();
     
     @Override
     public void initialize(URL location, ResourceBundle rb) {
          try {
         setTable();
-        dateTimeLabel.setText(String.valueOf(currentDate));
+        //Tardiness Table
+        dateCol.setCellValueFactory(new PropertyValueFactory<>("date"));
+        arCol.setCellValueFactory(new PropertyValueFactory<>("timeIn"));
+        notCol.setCellValueFactory(new PropertyValueFactory<>("notation"));
+        //Selection Name table
+        empName.setCellValueFactory(new PropertyValueFactory<>("name"));
+        
+        dateTimeLabel.setText(String.valueOf(currentMonth)+" "+currentDay+", "+ currentYear);
         ObservableList<String> monthList = FXCollections.observableArrayList();
         monthList.addAll("January", "February", "March", 
                 "April", "May", "June", "July", 
@@ -84,23 +98,27 @@ public class ADMIN_AttReportsCTRL implements Initializable{
     public void selectYear(){
         String selectedMonth = (String) monthChoiceBox.getSelectionModel().getSelectedItem();
         String selectedYear = (String) yearChoiceBox.getSelectionModel().getSelectedItem();
-        monthYearLabel.setText(selectedMonth+ ", "+selectedYear);
+        monthYearLabel.setText(selectedMonth+ " , "+selectedYear);
     }
     public void selectMonth(){
         String selectedMonth = (String) monthChoiceBox.getSelectionModel().getSelectedItem();
         String selectedYear = (String) yearChoiceBox.getSelectionModel().getSelectedItem();
-        monthYearLabel.setText(selectedMonth+ ", "+selectedYear);
+        monthYearLabel.setText(selectedMonth+ " , "+selectedYear);
     }
     
     public void setTable(){
+        
         empNameTable.setItems(Attendance.getEmpName());
         monthChoiceBox.setValue(capitalizedMonth);
         yearChoiceBox.setValue(currentYear+"");
         searchBar.setText("");
-        monthYearLabel.setText("");
         nameLabel.setText("");
+        totalLogin.setText("");
+        byPercent.setText("0%");
+        totalLate.setText("");
         searchBar.setPromptText("Search name...");
-        empName.setCellValueFactory(new PropertyValueFactory<>("name"));
+        monthChoiceBox.setOnAction(event -> selectMonth());
+        yearChoiceBox.setOnAction(event -> selectYear());
     }
     @FXML
     private void filterBySearchBar(KeyEvent event) {
@@ -114,6 +132,49 @@ public class ADMIN_AttReportsCTRL implements Initializable{
                 }
         empNameTable.setItems(filteredData);
     }
+    @FXML
+    private void setTardinessTable(ActionEvent event) {
+        ObservableList<Attendance> filteredData = FXCollections.observableArrayList();
+        String selectedYear = (String) yearChoiceBox.getSelectionModel().getSelectedItem();
+        String[] dateOnCTRL = monthYearLabel.getText().split(" ");
+        String selectedName = nameLabel.getText();
+        String monthToNum = "",converted ="";
+        int count=0, daysInMonth=0;
+        double tardiness;
+            for (Attendance attendance : getAttendancebyLate()){
+                String[] dateOnModel = attendance.getDate().toString().split("-");
+                if(dateOnCTRL[0].equals("January")){monthToNum="1";}if(dateOnCTRL[0].equals("February")){monthToNum="2";}
+                if(dateOnCTRL[0].equals("March")){monthToNum="3";}if(dateOnCTRL[0].equals("April")){monthToNum="4";}
+                if(dateOnCTRL[0].equals("May")){monthToNum="5";}if(dateOnCTRL[0].equals("June")){monthToNum="6";}
+                if(dateOnCTRL[0].equals("July")){monthToNum="7";}if(dateOnCTRL[0].equals("August")){monthToNum="8";}
+                if(dateOnCTRL[0].equals("September")){monthToNum="9";}if(dateOnCTRL[0].equals("October")){monthToNum="10";}
+                if(dateOnCTRL[0].equals("November")){monthToNum="11";}if(dateOnCTRL[0].equals("December")){monthToNum="12";}
+                   
+                int year= Integer.parseInt(dateOnCTRL[2]);
+                int month = Integer.parseInt(monthToNum);
+                YearMonth yearMonth = YearMonth.of(year, month);
+                daysInMonth = yearMonth.lengthOfMonth();
+                System.out.println("Number of days in " + yearMonth + ": " + daysInMonth);
+                     
+                for(int i =1; i<=daysInMonth; i++){
+                    if (attendance.getName().equals(selectedName) && dateOnModel[0].equals(dateOnCTRL[2])
+                        && dateOnModel[1].equals(monthToNum) && dateOnModel[2].equals(i+"")){
+                         count++;
+                        filteredData.add(attendance);
+                        i=32;
+                    }
+                } 
+            }
+            daysInMonth = (daysInMonth*2);
+            if(count!=0){ 
+                tardiness = ((double) count / (double) (daysInMonth)) * 100;
+                converted = String.format("%.1f", tardiness);
+            }else{converted = "0";}
+            if(!nameLabel.getText().isEmpty()){totalLogin.setText(daysInMonth+"");totalLate.setText(count+"");}
+        tardinessTable.setItems(filteredData);
+        byPercent.setText(converted+"%");
+    } 
+    
     @FXML
     private void resetSelectedItems(ActionEvent event){
         setTable();
